eace90b29cbff965cd377dd0e871a0fbdbde67e2
tuanaiseo <tuanaiseo@gmail.com>
2026-04-07 06:24:06 +0700

    fix(security)(materials): stored xss in zhuanlan component content block
    The component renders `content` directly into the DOM using `dangerouslySetInnerHTML`. Because this value is editable content, malicious HTML can be persisted and executed for any viewer.
    
    Affected files: index.tsx, index.tsx
    
    Signed-off-by: tuanaiseo <221258316+tuanaiseo@users.noreply.github.com>

--- src/materials/base/RichText/index.tsx
+++ src/materials/base/RichText/index.tsx
@@ -7,8 +7,83 @@ interface IProps extends IButtonConfig {
   isTpl: boolean;
 }
 
+const sanitizeHtml = (html = "") => {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+
+  const allowedTags = new Set([
+    "a",
+    "abbr",
+    "b",
+    "blockquote",
+    "br",
+    "code",
+    "div",
+    "em",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "hr",
+    "i",
+    "img",
+    "li",
+    "ol",
+    "p",
+    "pre",
+    "span",
+    "strong",
+    "u",
+    "ul"
+  ]);
+  const allowedAttrs = new Set([
+    "alt",
+    "class",
+    "href",
+    "rel",
+    "src",
+    "style",
+    "target",
+    "title"
+  ]);
+
+  const clean = (node: Node) => {
+    if (node.nodeType === 1) {
+      const el = node as HTMLElement;
+      const tag = el.tagName.toLowerCase();
+      if (!allowedTags.has(tag)) {
+        el.remove();
+        return;
+      }
+
+      Array.from(el.attributes).forEach(attr => {
+        const name = attr.name.toLowerCase();
+        const value = attr.value.trim().toLowerCase();
+        if (
+          name.indexOf("on") === 0 ||
+          !allowedAttrs.has(name) ||
+          ((name === "href" || name === "src") &&
+            (value.indexOf("javascript:") === 0 ||
+              value.indexOf("data:") === 0 ||
+              value.indexOf("vbscript:") === 0))
+        ) {
+          el.removeAttribute(attr.name);
+        }
+      });
+    }
+
+    Array.from(node.childNodes).forEach(clean);
+  };
+
+  Array.from(template.content.childNodes).forEach(clean);
+  return template.innerHTML;
+};
+
 const XButton = memo((props: IProps) => {
   const { isTpl, borderColor, borderWidth, round, padding, content } = props;
+  const safeContent = sanitizeHtml(content);
 
   return isTpl ? (
     <div>
@@ -23,7 +98,7 @@ const XButton = memo((props: IProps) => {
         padding: padding + "px"
       }}
     >
-      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      <div dangerouslySetInnerHTML={{ __html: safeContent }}></div>
     </div>
   );
 });
